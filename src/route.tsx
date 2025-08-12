cecd7ca7c91fda626d2cf6f6f846c89c6cc719c3
Kuldeep <Kuldeepraj5081@gmail.com>
2025-08-13 00:13:38 +0530

    fixed

--- src/route.tsx
+++ src/route.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Location from "./Page/Location";
-import MainLayout from "./components/layout/MainLayout"
+import MainLayout from "./components/layout/MainLayout";
 
 export const router = createBrowserRouter([
   {
