d00e15e83f728419853a160ed9b0987c09dd2f4f
Kuldeep <Kuldeepraj5081@gmail.com>
2025-08-12 23:15:06 +0530

    Revert "fix build"
    This reverts commit 8a10bf8fbcaf0f9d8cebaefae4bf94cb256e72e4.

--- src/components/Layout/MainLayout.tsx
+++ src/components/Layout/MainLayout.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
-import Header from "./Header";
-import Footer from "./Footer";
+import Header from "../layout/Header";
+import Footer from "../layout/Footer";
 
 export default function MainLayout() {
   return (
